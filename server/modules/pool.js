ecdc668a44f06bfa7305db2bb0f3414a6d2305de
Sam Johnson <s_johnso@yahoo.com>
2023-07-31 16:16:46 -0500

    update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,5 +1,15 @@
 const pg = require("pg");
-let pool = new pg.Pool({
+let pool; 
+
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else
+    pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: 'weekend_to_do_app'
